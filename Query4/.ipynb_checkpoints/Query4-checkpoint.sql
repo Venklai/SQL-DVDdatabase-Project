47b4ead06170a178da1e2e9dc18f41221fb06aba
Venklai <141069443+Venklai@users.noreply.github.com>
2023-09-05 23:49:08 -0400

    Query and result

--- Query4/.ipynb_checkpoints/Query4-checkpoint.sql
+++ Query4/.ipynb_checkpoints/Query4-checkpoint.sql
@@ -1,5 +1,5 @@
 --Calculate average amount that customer spent on DVD rental--
-SELECT p.customer_id, c.first_name, c.last_name, ROUND(AVG(p.amount),3) AS avg_amount 
+SELECT p.customer_id, c.first_name, c.last_name, AVG(p.amount) AS avg_amount 
 FROM payment p
 LEFT JOIN customer c ON p.customer_id = c.customer_id
 GROUP BY p.customer_id, c.first_name, c.last_name
